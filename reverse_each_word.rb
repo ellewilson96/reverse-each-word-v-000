d2e5d31e8dd988054841571e502deeaa00805926
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 17:29:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,6 @@
-
-
 def reverse_each_word(sentence)
-  sentence = []
-  sentence.collect do |reversed|
-    reversed.reverse
-    sentence << reversed
-    puts "#{sentence}"
-  end
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
