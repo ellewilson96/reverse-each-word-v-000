343a2f4f4b71788c7e508eb64c0d404ad62264d2
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 17:12:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@
 def reverse_each_word(sentence)
   sentence = []
   sentence.collect do |reversed|
-    sentence << reversed.reverse 
+    sentence << reversed.reverse
     puts sentence
   end
 end
