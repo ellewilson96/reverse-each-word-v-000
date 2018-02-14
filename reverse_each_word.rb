4ba5609fca606808de05348523a74dd0b7e7234e
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 17:09:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 
 def reverse_each_word(sentence)
   sentence = []
-  sentence.collect do |reversed|
+  sentence.each do |reversed|
     reversed.reverse << sentence
     puts "I love #{reversed} on my burgers!"
   end
